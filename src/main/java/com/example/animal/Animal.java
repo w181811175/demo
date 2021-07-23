89b3b08de29f62e6dd36aec725cd93c88a2cbbbf
wangling <475756754@qq.com>
2021-07-23 10:17:28 +0800

    初始化版本库

---
+++ src/main/java/com/example/animal/Animal.java
@@ -0,0 +1,9 @@
+package com.example.animal;
+
+public class Animal {
+
+    void walk(){
+        System.out.println("I am walking");
+    }
+
+}
---
+++ src/main/java/com/example/animal/Solution.java
@@ -0,0 +1,12 @@
+package com.example.animal;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+
+}
